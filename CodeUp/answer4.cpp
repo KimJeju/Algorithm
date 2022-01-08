b16bd9ce6a5bf829e5cc8f7f752ad4d09160b9a5
Kimjeju <kingswingsex@gmail.com>
2022-01-08 17:59:21 +0900

    테스트용

--- CodeUp/answer4.cpp
+++ CodeUp/answer4.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
 int main() {
-   std::cout << "\'Hello\'" << endl;
+   std::cout << "\'Hello\'" << std::endl;
 }
+
+// 'Hello' 출력하기
---
+++ CodeUp/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int iNumber = 0;
+
+iNumber = 0;
+	while (iNumber > 10)
+	{
+		cout << iNumber << endl;
+		++iNumber;
+	}
+
+}    
