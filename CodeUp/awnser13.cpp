6b541d76930a938d591b1f286a1084c053706b2b
KunHwi kim <80661467+KimJeju@users.noreply.github.com>
2022-01-10 10:36:36 +0900

    갈 수 록 어려워짐 ㅠ

---
+++ CodeUp/awnser13.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float a ;
+  cout << fixed;
+  cout.precision(2);
+
+  cin >> a;
+  cout << a << endl;
+
+}
+
+//float 함수에서 값을 받아 소수점 2자리 까지 반환하는 예제
