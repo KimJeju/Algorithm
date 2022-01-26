d5baa519a87c09225a3cc4297290108bafa40c43
KunHwi kim <80661467+KimJeju@users.noreply.github.com>
2022-01-26 22:27:28 +0900

    20 ~ 21 추가

---
+++ CodeUp/awnser22.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m,y,d;
+    scanf("%d.%d.%d",&m,&y,&d);
+    printf("%04d.%02d.%02d",m,y,d);
+    return 0;
+}
+
+//1019 : [기초-입출력] 년월일 입력받아 형식에 맞게 출력하기
+
+
