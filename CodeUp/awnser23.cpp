307080d8f9f2a9923c18b62e50b5a1bf40a3ad99
KunHwi kim <80661467+KimJeju@users.noreply.github.com>
2022-01-27 15:51:51 +0900

    22~23 추가

---
+++ CodeUp/awnser23.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e;
+    scanf("%1d%1d%1d%1d%01d",&a,&b,&c,&d,&e);
+    printf("[%d]\n", a * 10000);
+    printf("[%d]\n", b * 1000);
+    printf("[%d]\n", c * 100);
+    printf("[%d]\n", d * 10);
+    printf("[%d]\n", e);
+    
+    return 0;
+
+}
+
+//1025 : [기초-입출력] 정수 한 개 입력받아 나누어 출력하기
+
