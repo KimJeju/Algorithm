272fbe8aa19ddf2704b1e26134d9daa925fc699f
KimJeju <kingswingsex@gmail.com>
2022-05-30 22:18:30 +0900

    백준 10430

--- BackJoon/BackJoon/src/backJoon_10430.java
+++ BackJoon/BackJoon/src/backJoon_10430.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class backJoon_10430 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+        scanner.close();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C)+(B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C)*(B*C))%C);
+
+
+
+    }
 }
