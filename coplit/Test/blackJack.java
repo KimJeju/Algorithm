d7f2078ef48d44194f1dfec9ab0fac282e03aa05
KimJeju <kingswingsex@gmail.com>
2022-06-04 22:23:59 +0900

    코플릿 순열문제

---
+++ coplit/Test/blackJack.java
@@ -0,0 +1,35 @@
+import java.util.Enumeration;
+
+public class blackJack {
+    public static void main(String[] args) {
+
+    }
+
+    public static int BlackJack(int[] cards) {
+        int count = 0;
+        //int length = cards.length;
+
+        for (int i = 1; i < cards.length; ++i) {
+            for(int j = i + 1; j < cards.length; ++j){
+                for(int k = j + 1; k < cards.length; ++k){
+                    int sum = cards[i] + cards[j] + cards[k];
+
+                    if(isPrime(sum)){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static Boolean isPrime(int number) {
+        for (int i = 2; i<=(int)Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
