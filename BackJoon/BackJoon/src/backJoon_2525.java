642da1c3ce67ac80fe8803658414be6277abb098
KimJeju <Kingswingsex@gmail.com>
2022-06-08 19:11:37 +0900

    백준 2525 풀이중

--- BackJoon/BackJoon/src/backJoon_2525.java
+++ BackJoon/BackJoon/src/backJoon_2525.java
@@ -9,19 +9,30 @@ public class backJoon_2525 {
         int minutes = scanner.nextInt();
         int time = scanner.nextInt();
 
-        if((time + minutes) >59){
+        if((time + minutes) >= 60){
             ++hour;
-            minutes = 60 - (minutes - time);
+            minutes = (minutes + time) - 60;
 
-            if(hour < 0){
-                hour = 23;
+            //만약 hour가 24시와 같거나 넘을 시 0으로 초기화 || 예) 23 48분 .. 25 -> 0시 13분
+
+            if(minutes > 59){ //만약 분 + 받은분 이 60과 같거나 크다면 시간을 한시간 더 늘려주고 분에 60을 빼준다
+                ++hour;
+                minutes = minutes - 60; //minutes = minutes - 60 과 같다 || -60을 해줌으로서 만약 분이 80일 경우 20분으로 만들어 줄 수 있다.
             }
 
-
+            if(hour > 23){
+                hour = 0;
+            }
 
             System.out.println(hour + " " + minutes);
         }else{
+            if(hour >= 24){
+                hour = 0;
+            }
             System.out.println(hour + " " + (minutes + time));
         }
     }
 }
+
+
+// 1000 / 60, 1000 % 60
---
+++ BackJoon/BackJoon/src/backJoon_2525_Reference.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class backJoon_2525_Reference {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int h = scanner.nextInt();
+        int m = scanner.nextInt();
+        int time = scanner.nextInt();
+
+        int a = h * 60 + m + + time; //모두 분으로 초기화 해준다.
+        h = a/60;
+        m = a%60;
+        
+
+        if(h >= 24){
+            h = h - 24; // 만약 시간이 24와 같거나 크다면 시간 -24
+        }               //0으로 초기화 하지않은 이유는 1시나 2시 같은 경우가 나올 수 도있기에
+
+
+
+    }
+}
