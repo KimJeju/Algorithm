61d251fd79be64870ae40ab756fc89e3c3346717
KimJeju <Kingswingsex@gmail.com>
2022-06-21 11:35:39 +0900

    암호화 문제

---
+++ coplit/Test/decryptCaesarCipher.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class decryptCaesarCipher {
+    public static void main(String[] args) {
+
+        System.out.println(decryptCaesarCipher("hello",3));
+    }
+
+    public static String decryptCaesarCipher(String str, int secret){
+
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+         
+        String result = "";
+
+        for(int i = 0; i < str.length(); ++i){
+            if(str.charAt(i) == ' '){
+                result += str.charAt(i);
+            }else{
+                int prev = alpha.indexOf(str.charAt(i));
+                int next = (prev - secret + alpha.length()) % alpha.length();
+
+                result += alpha.charAt(next);
+            }
+        }
+
+        return result;
+    }
+}
